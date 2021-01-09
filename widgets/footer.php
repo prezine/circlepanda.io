a7ac91bc438eac9fe9bbc51f4365ad714cf27e24
Precious Tom <tomprezine@gmail.com>
2021-01-09 21:40:59 +0100

    initial upload

---
+++ widgets/footer.php
@@ -0,0 +1,109 @@
+<footer class="footer py-8 py-md-11 bg-dark border-top border-gray-900">
+    <div class="container-lg">
+      <div class="row">
+        <div class="col-md-4">
+
+          <!-- Brand -->
+          <h2 class="font-family-serif text-white mb-1">
+            <img src="./img/logo-2.png" alt="">
+          </h2>
+
+          <!-- Text -->
+          <p class="text-white-60">
+            A better way to build.
+          </p>
+
+        </div>
+        <div class="col-6 col-md">
+
+          <!-- Heading -->
+          <h6 class="text-uppercase text-white mb-3 mb-md-5">
+            Services
+          </h6>
+
+          <!-- List -->
+          <ul class="list-unstyled">
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">Help Center</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">Press</a>
+            </li>
+            <li>
+              <a class="text-white-60" href="index.html#!">About us</a>
+            </li>
+          </ul>
+
+        </div>
+        <div class="col-6 col-md">
+
+          <!-- Heading -->
+          <h6 class="text-uppercase text-white mb-3 mb-md-5">
+            Products
+          </h6>
+
+          <!-- List -->
+          <ul class="list-unstyled">
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">Homepage</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">Pages</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">Legal</a>
+            </li>
+            <li>
+              <a class="text-white-60" href="index.html#!">Terms</a>
+            </li>
+          </ul>
+
+        </div>
+        <div class="col-6 col-md">
+
+          <!-- Heading -->
+          <h6 class="text-uppercase text-white mb-3 mb-md-5">
+            Services
+          </h6>
+
+          <!-- List -->
+          <ul class="list-unstyled">
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">Help Center</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">Press</a>
+            </li>
+            <li>
+              <a class="text-white-60" href="index.html#!">About us</a>
+            </li>
+          </ul>
+
+        </div>
+        <div class="col-6 col-md">
+
+          <!-- Heading -->
+          <h6 class="text-uppercase text-white mb-3 mb-md-5">
+            Connect
+          </h6>
+
+          <!-- List -->
+          <ul class="list-unstyled">
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">Help Center</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">Press</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-white-60" href="index.html#!">About us</a>
+            </li>
+            <li>
+              <a class="text-white-60" href="index.html#!">Pages</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ widgets/header.php
@@ -0,0 +1,343 @@
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-lg">
+
+    <!-- Brand -->
+    <a class="navbar-brand d-lg-none" href="./">
+      <img src="./assets/img/logo.png" alt="" class="__logo">
+    </a>
+
+    <!-- Toggler -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Collapse -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <!-- Navigation -->
+      <ul class="navbar-nav justify-content-end w-100">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="landingsDropdown" href="index.html#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Products
+          </a>
+          <div class="dropdown-positioner">
+            <div class="dropdown-menu dropdown-menu-xl dropdown-menu-flush" aria-labelledby="landingsDropdown">
+              <div class="row align-items-center no-gutters">
+                <div class="col-md-6 order-1 dropdown-menu-col rounded-top-left-0 bg-gray-900 pt-4 py-lg-5">
+                  <div class="dropdown-menu-body">
+
+                    <!-- Heading -->
+                    <h6 class="dropdown-header text-uppercase text-primary-light d-none d-lg-block">
+                      Coming soon
+                    </h6>
+
+                    <!-- Links -->
+                    <a class="d-flex text-decoration-none" href="coworking.html">
+
+                      <!-- Icon -->
+                      <div class="icon icon-sm text-primary-light">
+                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M5.133 2.036c6.945 3.876 9.445 8.206 7.5 12.99-4.97-.79-7.47-5.12-7.5-12.99z" fill="#335EEA" opacity=".3"/><path d="M18.742 2.036c-6.945 3.876-9.445 8.206-7.5 12.99 4.97-.79 7.47-5.12 7.5-12.99z" fill="#335EEA" opacity=".3"/><path d="M12 1c-4.076 6.83-4.076 11.83 0 15 3.91-3.17 3.91-8.17 0-15z" fill="#335EEA" opacity=".3"/><path d="M6.344 13h11.312a1 1 0 01.958 1.287l-2.186 7.288A2 2 0 0114.512 23H9.488a2 2 0 01-1.916-1.425l-2.186-7.288A1 1 0 016.344 13z" fill="#335EEA"/></g></svg>
+                      </div>
+
+                      <div class="ml-3">
+
+                        <!-- Heading -->
+                        <p class="font-weight-bold text-white mb-0">
+                          Pandamanager
+                        </p>
+
+                        <!-- Text -->
+                        <span class="text-gray-500">
+                          Complete Business Manager, with 360 access all round to all panda products
+                        </span>
+
+                      </div>
+
+                    </a>
+                    <a class="d-flex text-decoration-none mt-4" href="course.html">
+
+                      <!-- Icon -->
+                      <div class="icon icon-sm text-primary-light">
+                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M6 3h12a2 2 0 012 2v14a2 2 0 01-2 2H6a2 2 0 01-2-2V5a2 2 0 012-2zm-.5 2a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zm12 0a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zm-12 4a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zm12 0a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zm-12 4a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zm12 0a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zm0 4a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zm-12 0a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1z" fill="#335EEA" opacity=".3"/><path d="M11.352 14.572l2.605-1.78a.5.5 0 00.001-.825l-2.605-1.792a.5.5 0 00-.783.412v3.572a.5.5 0 00.782.413z" fill="#335EEA"/></g></svg>
+                      </div>
+
+                      <div class="ml-3">
+
+                        <!-- Heading -->
+                        <p class="font-weight-bold text-white mb-0">
+                          Pandalearn
+                        </p>
+
+                        <!-- Text -->
+                        <span class="text-gray-500">
+                          Innovative learning space for all age, & gender and culture
+                        </span>
+
+                      </div>
+
+                    </a>
+                    <a class="d-flex text-decoration-none mt-4" href="software-automation.html">
+
+                      <!-- Icon -->
+                      <div class="icon icon-sm text-primary-light">
+                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M8 7a1 1 0 110-2h8a4 4 0 110 8H8a2 2 0 100 4h9a1 1 0 010 2H8a4 4 0 110-8h8a2 2 0 100-4H8z" fill="#335EEA" opacity=".3"/><path d="M9.707 8.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 1.414L7.414 6l2.293 2.293zM14.293 20.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 00-1.414 1.414L16.586 18l-2.293 2.293z" fill="#335EEA"/></g></svg>
+                      </div>
+
+                      <div class="ml-3">
+
+                        <!-- Heading -->
+                        <p class="font-weight-bold text-white mb-0">
+                          CyberPanda
+                        </p>
+
+                        <!-- Text -->
+                        <span class="text-gray-500">
+                          Crypto Ecosystem, News, Trade, Wallet & Market.
+                        </span>
+
+                      </div>
+
+                    </a>
+
+                  </div>
+                </div>
+                <div class="col-md-6 order-0 dropdown-menu-col">
+                  <div class="dropdown-menu-body">
+
+                    <!-- Heading -->
+                    <h6 class="dropdown-header text-uppercase d-none d-lg-block">
+                      Software
+                    </h6>
+
+                    <!-- Links -->
+                    <a class="d-flex text-decoration-none text-reset" href="robo-advisor.html">
+
+                      <!-- Icon -->
+                      <div class="icon icon-sm text-primary-light">
+                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M4 4h16a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2z" fill="#335EEA" opacity=".3"/><path d="M18.5 11h-13A1.5 1.5 0 004 12.5v.5h4.586a1 1 0 01.707.293l1 1a2.414 2.414 0 003.414 0l1-1a1 1 0 01.707-.293H20v-.5a1.5 1.5 0 00-1.5-1.5zM5.5 6A1.5 1.5 0 004 7.5V8h16v-.5A1.5 1.5 0 0018.5 6h-13z" fill="#335EEA"/></g></svg>
+                      </div>
+
+                      <div class="ml-3">
+
+                        <!-- Heading -->
+                        <p class="font-weight-bold mb-0">
+                          Pandastudio
+                        </p>
+
+                        <!-- Text -->
+                        <span class="text-muted">
+                          A digital Tech Agency, taking businesses brands to the next level.
+                        </span>
+
+                      </div>
+
+                    </a>
+                    <a class="d-flex text-decoration-none text-reset mt-4" href="analytics-saas.html">
+
+                      <!-- Icon -->
+                      <div class="icon icon-sm text-primary-light">
+                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M4.002 12.2L13 14V4.062A8.001 8.001 0 0112 20a8 8 0 01-7.998-7.8z" fill="#335EEA" opacity=".3"/><path d="M3.06 10.012A8.001 8.001 0 0111 3v8.6l-7.94-1.588z" fill="#335EEA"/></g></svg>
+                      </div>
+
+                      <div class="ml-3">
+
+                        <!-- Heading -->
+                        <p class="font-weight-bold mb-0">
+                          Pandascrow
+                        </p>
+
+                        <!-- Text -->
+                        <span class="text-muted">
+                          Modern Escrow System for Africa, Protect businesses from fraud.
+                        </span>
+
+                      </div>
+
+                    </a>
+                    <a class="d-flex text-decoration-none text-reset mt-4" href="e-commerce-platform.html">
+
+                      <!-- Icon -->
+                      <div class="icon icon-sm text-primary-light">
+                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M18.145 11.844l-.698 4.185a1 1 0 01-1.165.82L4.913 14.78a1 1 0 01-.783-.709L2.306 7.685a1 1 0 01.961-1.275h13.726l.613-2.627a1.18 1.18 0 011.15-.912h2.064a1.18 1.18 0 110 2.36h-1.128l-1.547 6.613z" fill="#335EEA" opacity=".3"/><path d="M6.5 21a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm9 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3z" fill="#335EEA"/></g></svg>
+                      </div>
+
+                      <div class="ml-3">
+
+                        <!-- Heading -->
+                        <p class="font-weight-bold mb-0">
+                          Pandashop
+                        </p>
+
+                        <!-- Text -->
+                        <span class="text-muted">
+                          A Flexible Space for Sales and Vendors, from Big or Small Businesses.
+                        </span>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="pagesDropdown" href="index.html#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Company
+          </a>
+          <div class="dropdown-positioner">
+            <div class="dropdown-menu dropdown-menu-lg" aria-labelledby="pagesDropdown">
+              <div class="row no-gutters">
+                <div class="col">
+
+                  <!-- Heading -->
+                  <h6 class="dropdown-header text-uppercase mb-3">
+                    Circlepanda
+                  </h6>
+
+                  <!-- Links -->
+                  <a class="dropdown-item" href="about.html">
+                    About us
+                  </a>
+                  <a class="dropdown-item" href="pricing.html">
+                    Compliance
+                  </a>
+                  <a class="dropdown-item" href="career-listing.html">
+                    Careers
+                  </a>
+                  <a class="dropdown-item" href="faq.html">
+                    FAQ
+                  </a>
+                  <a class="dropdown-item" href="contact.html">
+                    Contact us
+                  </a>
+                  <a class="dropdown-item" href="career-benefits.html">
+                    Brand
+                  </a>
+                  <a class="dropdown-item" href="career-benefits.html">
+                    Media kit
+                  </a>
+
+                </div>
+                <div class="col">
+
+                  <!-- Heading -->
+                  <h6 class="dropdown-header text-uppercase mb-3">
+                    Learn
+                  </h6>
+
+                  <!-- Links -->
+                  <a class="dropdown-item" href="blog-listing.html">
+                    Blog
+                  </a>
+                  <a class="dropdown-item" href="blog-post.html">
+                    Guide
+                  </a>
+                  <a class="dropdown-item" href="blog-post-cover.html">
+                    Testimonies
+                  </a>
+
+                  <!-- Heading -->
+                  <h6 class="dropdown-header text-uppercase mb-3">
+                    Help Center
+                  </h6>
+
+                  <!-- Links -->
+                  <a class="dropdown-item" href="help-center.html">
+                    Quick help
+                  </a>
+                  <a class="dropdown-item" href="help-center-article.html">
+                    Support
+                  </a>
+
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </li>
+      </ul>
+
+      <!-- Brand -->
+      <a class="navbar-brand d-none d-lg-block" href="./">
+        <img src="./assets/img/logo.png" alt="" class="__logo">
+      </a>
+
+      <!-- Navigation -->
+      <ul class="navbar-nav justify-content-start w-100">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="accountDropdown" href="index.html#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Why Circlepanda
+          </a>
+          <div class="dropdown-positioner">
+            <ul class="dropdown-menu" aria-labelledby="accountDropdown">
+              <li>
+                  <a class="dropdown-item" href="pricing.html">
+                    For Entrepreneurs
+                  </a>
+                  <a class="dropdown-item" href="pricing.html">
+                    For Businesses
+                  </a>
+                  <a class="dropdown-item" href="pricing.html">
+                    For Schools
+                  </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="docsDropdown" href="index.html#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Developer
+          </a>
+          <div class="dropdown-positioner">
+            <div class="dropdown-menu dropdown-menu-lg" aria-labelledby="docsDropdown">
+              <a class="d-flex align-items-center text-reset text-decoration-none" href="docs/index.html">
+
+                <!-- Icon -->
+                <div class="icon text-primary-light">
+                  <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M8 3v.5A1.5 1.5 0 009.5 5h5A1.5 1.5 0 0016 3.5V3h2a2 2 0 012 2v16a2 2 0 01-2 2H6a2 2 0 01-2-2V5a2 2 0 012-2h2z" fill="#335EEA" opacity=".3"/><path d="M11 2a1 1 0 012 0h1.5a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-5a.5.5 0 01-.5-.5v-1a.5.5 0 01.5-.5H11z" fill="#335EEA"/><rect fill="#335EEA" opacity=".3" x="7" y="10" width="5" height="2" rx="1"/><rect fill="#335EEA" opacity=".3" x="7" y="14" width="9" height="2" rx="1"/></g></svg>
+                </div>
+
+                <div class="ml-3">
+
+                  <!-- Heading -->
+                  <h6 class="text-uppercase text-primary mb-0">
+                    Documentation
+                  </h6>
+
+                  <!-- Text -->
+                  <span>Learn how to integrate any product from Circlepanda.io</span>
+
+                </div>
+
+              </a>
+              <a class="d-flex align-items-center text-reset text-decoration-none mt-5" href="docs/changelog.html">
+
+                <!-- Icon -->
+                <div class="icon text-primary-light">
+                  <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M5.857 2h7.88a1.5 1.5 0 01.968.355l4.764 4.029A1.5 1.5 0 0120 7.529v12.554c0 1.79-.02 1.917-1.857 1.917H5.857C4.02 22 4 21.874 4 20.083V3.917C4 2.127 4.02 2 5.857 2z" fill="#335EEA" opacity=".3"/><rect fill="#335EEA" x="6" y="11" width="9" height="2" rx="1"/><rect fill="#335EEA" x="6" y="15" width="5" height="2" rx="1"/></g></svg>
+                </div>
+
+                <div class="ml-3">
+
+                  <!-- Heading -->
+                  <h6 class="text-uppercase text-primary mb-0">
+                    Status Page
+                  </h6>
+
+                  <!-- Text -->
+                  <span>Track the status of all apps from Circlepanda.io </span>
+
+                </div>
+
+              </a>
+            </div>
+
+          </div>
+        </li>
+      </ul>
+
+    </div>
+
+  </div>
+</nav>
